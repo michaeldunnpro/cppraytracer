f936ec34d68204fad11d4d4698f09d08a7cdabd8
michaeldunnpro <michael.dunn.pro@gmail.com>
2025-12-07 11:47:18 -0800

    Reorganized scene setup and added comments. Still will need to document.

--- scenes/example_scene.cpp
+++ scenes/example_scene.cpp
@@ -15,24 +15,27 @@
 // Point light at (0, -0.5, 1)
 
 int main() {
+    // Create scene components (Must do this first)
     auto camera = cam(Point(0.5f, -1.5f, 0.5f), Vector(0.0f, 1.0f, 0.0f));
     auto scr = screen(10.0f, 10.0f);
     auto scn = scene(camera, scr, 0.8f, 0.5f, 8.0f, Color(135, 206, 235));
 
+    // Define materials
     auto red = mat(Color(255, 0, 0), 0.4f);
-    sphere(Point(0.25f, 0.45f, 0.4f), 0.1f, red, scn);
-
     auto green = mat(Color(0, 255, 0), 0.2f);
-    sphere(Point(1.0f, 1.0f, 0.25f), 0.25f, green, scn);
-
     auto blue = mat(Color(0, 0, 255), 0.7f);
-    sphere(Point(0.8f, 0.3f, 0.15f), 0.15f, blue, scn);
-
     auto gray = mat(Color(200, 200, 200), 0.5f);
-    plane(Point(0.0f, 0.0f, -0.1f), Vector(0.0f, 0.0f, 1.0f), gray, scn);
 
+    // Add shapes to the scene
+    sphere(Point(0.25f, 0.45f, 0.4f), 0.1f, red, scn);
+    sphere(Point(1.0f, 1.0f, 0.25f), 0.25f, green, scn);
+    sphere(Point(0.8f, 0.3f, 0.15f), 0.15f, blue, scn);
+    plane(Point(0.0f, 0.0f, -0.1f), Vector(0.0f, 0.0f, 1.0f), gray, scn);
+    
+    // Add point light to the scene
     scn.add_point_light(Point(0.0, -0.5, 1.0));
 
+    // Handle user input and render the scene
     handle_input(scn);
     return 0;
 }
