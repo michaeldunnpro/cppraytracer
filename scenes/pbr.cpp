a62815add54d0cbf713ac5d1620eba46e19cd580
fuelatank <fuelatank@gmail.com>
2025-12-13 10:59:39 -0800

    Modifie colors in pbr.cpp

--- scenes/pbr.cpp
+++ scenes/pbr.cpp
@@ -3,9 +3,9 @@
 // TO RUN: make scene SCENE=scenes/pattern.cpp
 // Cam at (0.5, -1.5, 0.5) looking towards (0,1,0)
 // Screen 10x10 units with distance 10
-// Spheres at (0.25, 0.45, 0.4) radius 0.1 red metal
-//            (1, 1, 0.25) radius 0.25 green metal rough
-//            (0.8, 0.3, 0.15) radius 0.15 blue non-metal
+// Spheres at (0.25, 0.45, 0.4) radius 0.1 copper metal
+//            (1, 1, 0.25) radius 0.25 iron metal rough
+//            (0.8, 0.3, 0.15) radius 0.15 brown non-metal
 // Plane at z = -0.1 gray non-metal rough
 // Point light at (0, -0.5, 1) inverse-square
 
@@ -16,16 +16,16 @@ int main() {
     auto scn = scene(camera, scr, 0.1f, 0.5f, 8.0f, Color(135, 206, 235));
 
     // Define materials
-    PBRMaterial red(Color(255, 0, 0), 0.2f, 1.0f);
+    PBRMaterial copper(Color(0.95, 0.64, 0.54) * 255.0, 0.2f, 1.0f);
     // rough metal needs more samples for specular reflection
-    PBRMaterial green(Color(0, 255, 0), 0.5f, 1.0f, 0.5f, 256);
-    PBRMaterial blue(Color(0, 0, 255), 0.2f, 0.0f);
+    PBRMaterial iron(Color(0.56, 0.57, 0.58) * 255.0, 0.5f, 1.0f, 0.5f, 256);
+    PBRMaterial brown(Color(165, 42, 42), 0.2f, 0.0f);
     PBRMaterial gray(Color(200, 200, 200), 0.5f, 0.0f);
 
     // Add simple shapes to the scene
-    sphere(Point(0.25f, 0.45f, 0.4f), 0.1f, red, scn);
-    sphere(Point(1.0f, 1.0f, 0.25f), 0.25f, green, scn);
-    sphere(Point(0.8f, 0.3f, 0.15f), 0.15f, blue, scn);
+    sphere(Point(0.25f, 0.45f, 0.4f), 0.1f, copper, scn);
+    sphere(Point(1.0f, 1.0f, 0.25f), 0.25f, iron, scn);
+    sphere(Point(0.8f, 0.3f, 0.15f), 0.15f, brown, scn);
     plane(Point(0.0f, 0.0f, -0.1f), Vector(0.0f, 0.0f, 1.0f), gray, scn);
 
     // Add point light to the scene
